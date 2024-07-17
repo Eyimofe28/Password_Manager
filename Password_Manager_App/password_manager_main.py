b7c1f865591ec7dbc5a1fd9770276fb255575f75
Eyimofe Ajagunna <147433023+Eyimofe28@users.noreply.github.com>
2024-07-17 08:33:14 -0500

    password_manager_main.py (updated)

--- Password_Manager_App/password_manager_main.py
+++ Password_Manager_App/password_manager_main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 
@@ -30,22 +31,74 @@ def generate_password():
 
 
 def save():
-    website = website_entry.get()
+    website = website_entry.get().title()
     email = email_entry.get()
     password = password_entry.get()
+
+    new_data = {website: {
+        "Email": email,
+        "Password": password
+    }}
 
     if website and email and password:
         if messagebox.askokcancel(title=website, message=f"Email: {email}\n"
                                                          f"Password: {password}\n\n"
                                                          f"Are you satisfied with this info you've entered?"):
-            with open("data.txt", mode='a') as file:
-                file.write(f"\n{website} | {email} | {password}")
-
-            messagebox.showinfo(message='Info saved🤩', title='Success!')
-            website_entry.delete(0, END)
-            password_entry.delete(0, END)
+            # with open("data.txt", mode='r') as file:
+            #     file.write(f"\n{website} | {email} | {password}")
+            try:
+                with open("data.json", mode="r") as file:
+                    # Reading old data from a JSON file.
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open('data.json', mode='w') as file:
+                    json.dump(new_data, file, indent=4)
+                    successful_save()
+            else:
+                if website in data:
+                    if messagebox.askokcancel(message=f"You are about to replace the password for this site: {website}."
+                                              f"\nWould you still like to proceed?", title='Warning ⚠️'):
+                        # Appending new data to old data.
+                        data.update(new_data)
+                        with open('data.json', mode='w') as file:
+                            # Replacing old data with new data in JSON file.
+                            json.dump(data, file, indent=4)
+                        successful_save()
+                else:
+                    data.update(new_data)
+                    with open('data.json', mode='w') as file:
+                        json.dump(data, file, indent=4)
+                    successful_save()
     else:
         messagebox.showerror(message="Please make sure not to leave any entry field empty.", title="Oops")
+
+
+def successful_save():
+    messagebox.showinfo(message='Info saved :)', title='Success!')
+    website_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+
+# ---------------------------- FIND PASSWORD ------------------------------- #
+
+
+def find_password():
+    try:
+        with open('data.json', mode='r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Error", message="Sorry, no data file found.")
+    else:
+        website = website_entry.get().title()
+        if website:
+            if website in data:
+                email = data[website]["Email"]
+                password = data[website]["Password"]
+                messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+            else:
+                messagebox.showerror(message="No details for this website found.", title="Oh No :(")
+        else:
+            messagebox.showerror(message="Please make sure the website entry box isn't empty and try again.")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -62,9 +115,12 @@ canvas.grid(row=0, column=1, pady=15)
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
-website_entry = Entry(width=42)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=24)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
+
+search_button = Button(text='Search', bg='#1A3636', fg='white', activebackground='blue', command=find_password)
+search_button.grid(row=1, column=2, sticky='w')
 
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0, pady=10)
@@ -72,7 +128,7 @@ email_label.grid(row=2, column=0, pady=10)
 email_entry = Entry(width=42)
 email_entry.grid(row=2, column=1, columnspan=2)
 
-email_entry.insert(END, string="eyimofeajagunna@gmail.com")
+email_entry.insert(END, string="default email")
 
 password_label = Label(text='Password:')
 password_label.grid(row=3, column=0)
